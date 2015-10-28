15c1c6aafc946a16dd8b6d8ec12cb7dc79a3abcb
Pamir Mundt <pmr.mndt@gmail.com>
2015-10-28 14:06:39 +0200

    Arduino PID

--- main/motor_control/motor_control.ino
+++ main/motor_control/motor_control.ino
@@ -7,16 +7,19 @@ const int STBY = 8;     //Driver Standby
 
 volatile long int interval = 0;   //RPM calculation interval
 volatile long int timeold = 0;    //RPM calculation
+volatile long int timeold2 = 0;   //PID calculation
 volatile int RPM = 0;             //RPM
 volatile double velErr = 0;       //Velocity Error
+volatile double velErrPrev = 0;   //Previous Velocity Error
 volatile double control = 0;      //Control Signal
-double refVel = 12000;            //Reference RPM
-double Kp = 0.00025;              //PID - Kp
-double Ki = 0.0;                  //PID - Ki
-double Kd = 0.0;                  //PID - Kd
+volatile double integral = 0;     //PID -> integral
+volatile double derivative = 0;   //PID -> derivative
+volatile long int dt = 0;         //Delta Time - PID           
 
-volatile long int interval2 = 0;
-volatile long int timeold2 = 0;
+double refVel = 0;            //Reference RPM
+double Kp = 0.00025;             //PID - Kp
+double Ki = 0.0000001;           //PID - Ki
+double Kd = 0.0;                 //PID - Kd
 
 int i = 0;
 
@@ -50,7 +53,7 @@ void setup() {
 void loop() {
   //analogWrite(PWMA, 73); //Max 255 - 6V
   while(true){
-    Serial.println(interval2);
+    Serial.println(RPM);
     i++;
   }
 }
@@ -66,10 +69,15 @@ void encoderA_RPM(){
 
 //Timer2 interrupt at 500hz for PID
 ISR(TIMER2_COMPA_vect){
-  interval2 = micros() - timeold2;
+  dt = micros() - timeold2;
   timeold2 = micros();
+
+  //PID
   velErr = refVel - RPM;
-  control = Kp*velErr + control;
+  integral = integral + velErr*dt;
+  derivative = (velErr - velErrPrev)/dt; 
+  control = Kp*velErr + Ki*integral + Kd*derivative;
+  velErrPrev = velErr;
   
   //AIN2 -> LOW, AIN1 -> HIGH, CW
   //AIN2 -> HIGH, AIN1 -> LOW, CCW
@@ -82,13 +90,14 @@ ISR(TIMER2_COMPA_vect){
     //CCW
     digitalWrite(AIN2, HIGH);
     digitalWrite(AIN1, LOW);
-  }  
+  }
+  //Saturate
   if(control > 255){
     control = 255;
   }
   if(control < -255){
     control = -255;
   }
-  analogWrite(PWMA,abs((int)control));  
+  analogWrite(PWMA,abs((int)control));
 }
 
